bb7563e3b00f3baffa54c26e06d5648dbac8f9df
Derek Evans <DerekEvans47@gmail.com>
2024-01-28 20:03:21 -0500

    Updated hidden application for secret keys.

--- weather_hidden.py
+++ weather_hidden.py
@@ -1,1 +1,36 @@
+import requests
 
+def get_location():
+
+    state = input("Please enter a state: ")
+    city = input("Please enter a city: ")
+
+    LOC_URL = f"https://api.api-ninjas.com/v1/geocoding?city={city}&state={state}&country=US"
+    LOC_API_KEY = loc_key
+    response = requests.get(LOC_URL, headers={'X-Api-Key': loc_key})
+    if response.status_code == requests.codes.ok:
+        results = response.json()[0]
+        lat = results['latitude']
+        long = results['longitude']
+        return lat, long, city
+    else:
+        print("Error:", response.status_code, response.text)
+
+def get_weather():
+
+    lat, long, city = get_location()
+
+    WEATHER_API_KEY = weather_key
+    WEATHER_URL = f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={long}&appid={weather_key}&units=imperial"
+    
+    response = requests.get(WEATHER_URL)
+    if response.status_code == requests.codes.ok:
+        results = response.json()
+        coverage = results['weather'][0]['description']
+        temp = results['main']['temp']
+    else:
+        print("Error:", response.status_code, response.text)
+
+    print(f"Currently in {city} there are {coverage} and it is {temp}° Farenheit.")   
+
+get_weather()
